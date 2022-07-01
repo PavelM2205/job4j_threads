ec4ef0c67e53258228a76524a395ff3cdb884349
PavelM2205 <pavelm2205@mail.ru>
2022-07-01 18:04:21 +0700

    3. Immutable объекты [#267918]

--- src/main/java/ru/job4j/linked/Node.java
+++ src/main/java/ru/job4j/linked/Node.java
@@ -1,37 +1,19 @@
 package ru.job4j.linked;
 
-import java.io.*;
-
-public final class Node<T extends Serializable> implements Serializable {
+public final class Node<T> {
     private final Node<T> next;
     private final T value;
 
     public Node(Node<T> next, T value) {
-        this.next = (Node<T>) copyObject(next);
-        this.value = (T) copyObject(value);
+        this.next = next;
+        this.value = value;
     }
 
     public Node<T> getNext() {
-        return (Node<T>) copyObject(next);
+        return next;
     }
 
     public T getValue() {
-        return (T) copyObject(value);
-    }
-
-    private Object copyObject(Object object) {
-        Object result = null;
-        String store = "";
-        try (ByteArrayOutputStream out = new ByteArrayOutputStream();
-        ObjectOutputStream ous = new ObjectOutputStream(out);
-        ByteArrayInputStream input = new ByteArrayInputStream(store.getBytes());
-        ObjectInputStream ois = new ObjectInputStream(input)) {
-            ous.writeObject(object);
-            store = out.toString();
-            result = ois.readObject();
-        } catch (Exception exc) {
-            exc.printStackTrace();
-        }
-        return result;
+        return value;
     }
 }
