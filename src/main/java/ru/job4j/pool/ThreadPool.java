122740157777105fed0c0d9049afed17be62aceb
PavelM2205 <pavelm2205@mail.ru>
2022-07-06 11:58:34 +0700

    1. Реализовать ThreadPool [#1099]

---
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -0,0 +1,63 @@
+package ru.job4j.pool;
+
+import ru.job4j.producerconsumer.SimpleBlockingQueue;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThreadPool {
+    private final List<Thread> threads = new LinkedList<>();
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(10);
+
+    public ThreadPool() {
+        int size = Runtime.getRuntime().availableProcessors();
+        for (int i = 0; i < size; i++) {
+            threads.add(new PoolThread(tasks));
+        }
+    }
+
+    public void work(Runnable job) {
+        try {
+            tasks.offer(job);
+        } catch (InterruptedException exc) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public void shutdown() {
+        for (var thread : threads) {
+            thread.interrupt();
+        }
+    }
+
+    private static class PoolThread extends Thread {
+        private final SimpleBlockingQueue<Runnable> queue;
+
+        public PoolThread(SimpleBlockingQueue<Runnable> queue) {
+            this.queue = queue;
+            this.start();
+        }
+
+        @Override
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    queue.poll().run();
+                } catch (InterruptedException exc) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ThreadPool pool = new ThreadPool();
+        Runnable task1 = () -> System.out.println("task1");
+        Runnable task2 = () -> System.out.println("task2");
+        Runnable task3 = () -> System.out.println("task3");
+        pool.work(task1);
+        pool.work(task2);
+        pool.work(task3);
+        pool.shutdown();
+    }
+}
