fea0edeaa2e5cacb2c5f28b1e273673ff4d0dc9a
PavelM2205 <pavelm2205@mail.ru>
2022-07-18 13:35:04 +0700

    3.2. Откат изменений [#504830]03

---
+++ src/main/java/ru/job4j/Changed.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Changed {
+    public static void main(String[] args) {
+        System.out.println("Изменение 1");
+        System.out.println("Изменение 2");
+    }
+}
